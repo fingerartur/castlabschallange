588df80248e6a74d9686886e628df8a528a243ac
Artur Finger <artur.finger@nangu.tv>
2022-12-09 16:24:22 +0200

    chore: Reconfigured eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.css'
 
 export const App = () => {
   return (
@@ -9,5 +9,5 @@ export const App = () => {
         </h1>
       </header>
     </div>
-  );
+  )
 }
